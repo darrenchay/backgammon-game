3b17c7a9d675a492b6ca3dd03aba3674bbe4f0e5
Darren Chay <darrenchay@gmail.com>
2021-10-08 12:43:37 -0400

    added direction in the throw

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -21,13 +21,33 @@ public class Dice : MonoBehaviour
         if(Input.GetKeyDown (KeyCode.Space))
         {
             DiceNum.diceNumber = 0;
+
+            // Setting the spin for the dice roll
             float forceX = Random.Range(100, 5000);
             float forceY = Random.Range(100, 5000);
             float forceZ = Random.Range(100, 5000);
              
+            // Resetting dice position and rotation before throwing
             transform.position = new Vector3(0, 2, 0);
             transform.rotation = Quaternion.identity;
-            rb.AddForce(transform.up * 2000);
+
+            // Setting the throw force
+            rb.AddForce(transform.up * 1000);
+            int direction = Random.Range(1, 3);
+            if(direction == 1)
+            {
+                rb.AddForce(-transform.forward * 1000);
+            }
+            else if (direction == 2)
+            {
+                rb.AddForce(transform.right * 1000);
+            } 
+            else if (direction == 3)
+            {
+                rb.AddForce(-transform.right * 1000);
+            }
+
+            // Applying spin
             rb.AddTorque(forceX, forceY, forceZ);
 
         }
